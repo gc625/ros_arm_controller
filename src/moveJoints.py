adc3e8393fa1d1fdcb158f05e82280bdfef90c7d
gc625 <gabrielchan625@gmail.com>
2021-11-08 21:41:57 +0000

    combining image1+image2

--- src/moveJoints.py
+++ src/moveJoints.py
@@ -5,7 +5,16 @@ import sys
 import rospy
 import cv2
 import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
 from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
 
 
 
+
+class joint_mover(self):
+
+	rospy.init_node('move_joint', anonymous=True)
+
+	
