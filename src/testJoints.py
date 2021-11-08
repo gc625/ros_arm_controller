efcc1912d4fbb0c31c3aa877a6b4528b7b81f099
gc625 <gabrielchan625@gmail.com>
2021-11-08 18:22:32 +0000

    fixed bagging and time

--- src/testJoints.py
+++ src/testJoints.py
@@ -21,14 +21,19 @@ class joint_pub:
     rate = rospy.Rate(50)  # 50hz
     # initialize a publisher for end effector target positions
     target_pos_pub = rospy.Publisher("target_pos", Float64MultiArray, queue_size=10)
+    rospy.get_time()
     t0 = rospy.get_time()
 
 
     bag = rosbag.Bag('test.bag', 'w')
 
-
+    
     while not rospy.is_shutdown():
-        cur_time = np.array([rospy.get_time()]) - t0
+        rospy.get_time()
+        cur_time = rospy.get_time() - t0
+        
+        print("in here",cur_time)
+        print(t0)
         #y_d = float(6 + np.absolute(1.5* np.sin(cur_time * np.pi/100)))
         j2 = np.pi * 0.5 * np.sin(cur_time * np.pi / 15)
         j3 = np.pi * 0.5 * np.sin(cur_time * np.pi / 20) 
@@ -54,7 +59,9 @@ class joint_pub:
         robot_joint2_pub.publish(joint3)
         robot_joint3_pub.publish(joint4)
         rate.sleep()
-
+        
+        if cur_time == 3:
+            rospy.signal_shutdown("3 sec passed")
     bag.close()
 # run the code if the node is called
 if __name__ == '__main__':
