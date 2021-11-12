c7865393318f57617f636122a562c2630703b686
cameronkent1 <81260203+cameronkent1@users.noreply.github.com>
2021-11-12 15:24:39 +0000

    Add files via upload
    two camera angles combined

---
+++ src/combined.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+    # Defines publisher and subscriber
+    def __init__(self):
+        # initialize the node named image_processing
+        rospy.init_node('image_processing', anonymous=True)
+        # initialize a publisher to send images from camera1 to a topic named image_topic1
+        self.image_pub1 = rospy.Publisher("image_topic1", Image, queue_size=1)
+        # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+        self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback1)
+        # initialize a publisher to send images from camera2 to a topic named image_topic2
+        self.image_pub2 = rospy.Publisher("image_topic2", Image, queue_size=1)
+        # initialize a subscriber to recieve messages rom a topic named /robot/camera2/image_raw and use callback function to recieve data
+        self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw", Image, self.callback2)
+        # initialize the bridge between openCV and ROS
+        self.bridge = CvBridge()
+
+
+    def callback1(self, data):
+        # Recieve the image
+        try:
+            self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        # Publish the results
+        try:
+            self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
+
+        # Recieve data, process it, and publish
+
+    def callback2(self, data):
+        # Recieve the image
+        try:
+            self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        image = np.concatenate((self.cv_image1, self.cv_image2), axis=1)
+        im = cv2.imshow('camera1 and camera 2', image)
+        cv2.waitKey(1)
+        cv2.imwrite('camera1 and camera 2.png', self.cv_image2)
+        # Publish the results
+        try:
+            self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
+
+# call the class
+def main(args):
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if __name__ == '__main__':
+    main(sys.argv)
