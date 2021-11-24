75d74d65ca42f7f73c9e232afa0157e11beb774d
cameronkent1 <81260203+cameronkent1@users.noreply.github.com>
2021-11-24 17:01:00 +0000

    deleted redundant block

--- src/control.py
+++ src/control.py
@@ -30,11 +30,11 @@ class forward_kin:
     self.m1 = self.makeMatrix(4, self.joint_1 + (-np.pi / 2), 0, -np.pi / 2)
     self.m2 = self.makeMatrix(0, self.joint_3 + (-np.pi / 2), 3.2, (np.pi / 2))
     self.m3 = self.makeMatrix(0, self.joint_4, 2.8, 0)
-    
+
     self.finalFk = (self.m1 * self.m2 * self.m3)
-    
+
     print(self.finalFk)
-      
+
   def makeMatrix(d, t, r, a):
       M = np.matrix([
        [np.cos(t), -np.sin(t) * np.cos(a), np.sin(t) * np.sin(a), r * np.cos(t)],
@@ -43,23 +43,6 @@ class forward_kin:
        [0, 0, 0, 1]]
       )
       return M
-
-  # M1 = makeMatrix(4,1.3-np.pi/2,0,-np.pi/2)
-  # M2 = makeMatrix(0,1.5+(-np.pi/2),3.2,(np.pi/2))
-  # M3 = makeMatrix(0,-1,2.8,0)
-
-  j1, j3, j4 = 1.5, 1, -1.3
-
-  # d t r a
-  M1 = makeMatrix(4, j1 + (-np.pi / 2), 0, -np.pi / 2)
-  M2 = makeMatrix(0, j3 + (-np.pi / 2), 3.2, (np.pi / 2))
-  M3 = makeMatrix(0, j4, 2.8, 0)
-
-  #print(M1)
-  #print(M2)
-  #print(M3)
-
-  print(M1 * M2 * M3)
 
 
 # call the class
