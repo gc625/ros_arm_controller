65eeded3e50f6d7877cb9029f169c93ff8d85092
cameronkent1 <81260203+cameronkent1@users.noreply.github.com>
2021-11-24 21:06:00 +0000

    printing out FK in (x,y,z in meters)

--- src/control.py
+++ src/control.py
@@ -14,28 +14,32 @@ import math
 
 class forward_kin:
 
-  # Defines publisher and subscriber
+  # Defines subscriber
   def __init__(self):
     # initialize the node named image_processing
     rospy.init_node('forward_kinematics', anonymous=True)
-    self.joint_1 = rospy.Subscriber("joints_angle_1", Float64MultiArray, self.getActual)
-    self.joint_3 = rospy.Subscriber("joints_angle_3", Float64MultiArray, self.getActual)
-    self.joint_4 = rospy.Subscriber("joints_angle_4", Float64MultiArray, self.getActual)
+    self.joint_1 = rospy.Subscriber("joint_angle_1", Float64, self.callback1)
+    self.joint_3 = rospy.Subscriber("joint_angle_3", Float64, self.callback2)
+    self.joint_4 = rospy.Subscriber("joint_angle_4", Float64, self.callback3)
 
-  def getActual(self, data):
-    self.joint_1 = float(data.data[0])
-    self.joint_3 = float(data.data[1])
-    self.joint_4 = float(data.data[2])
+  def callback1(self, data):
+    self.joint1 = float(data.data)
 
-    self.m1 = self.makeMatrix(4, self.joint_1 + (-np.pi / 2), 0, -np.pi / 2)
-    self.m2 = self.makeMatrix(0, self.joint_3 + (-np.pi / 2), 3.2, (np.pi / 2))
-    self.m3 = self.makeMatrix(0, self.joint_4, 2.8, 0)
+  def callback2(self, data):
+    self.joint3 = float(data.data)
+
+  def callback3(self, data):
+    self.joint4 = float(data.data)
+
+    self.m1 = self.makeMatrix(4, self.joint1 + (-np.pi / 2), 0, -np.pi / 2)
+    self.m2 = self.makeMatrix(0, self.joint3 + (-np.pi / 2), 3.2, (np.pi / 2))
+    self.m3 = self.makeMatrix(0, self.joint4, 2.8, 0)
 
     self.finalFk = (self.m1 * self.m2 * self.m3)
 
     print(self.finalFk)
 
-  def makeMatrix(d, t, r, a):
+  def makeMatrix(self, d, t, r, a):
       M = np.matrix([
        [np.cos(t), -np.sin(t) * np.cos(a), np.sin(t) * np.sin(a), r * np.cos(t)],
        [np.sin(t), np.cos(t) * np.cos(a), -np.cos(t) * np.sin(a), r * np.sin(t)],
@@ -47,7 +51,7 @@ class forward_kin:
 
 # call the class
 def main(args):
-  fk = forward_kin()
+  forward_kin()
   try:
     rospy.spin()
   except KeyboardInterrupt:
